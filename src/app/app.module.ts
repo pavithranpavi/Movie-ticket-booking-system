cff663ec2d768a4a66457b2b884badd62d786f1d
pavi <pavithran.rathinam@mindzen.com>
2017-09-26 09:10:23 +0530

    Module error fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,17 @@ import { AppRoutes } from './app.routing';
 import { BookTicketsComponent } from './book-tickets/book-tickets.component';
 import { MyBookingComponent } from './my-booking/my-booking.component';
 
+import {CdkTableModule} from '@angular/cdk/table';
+
+import {MatTableModule} from '@angular/material';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import {MatMenuModule , MatIconModule, MatCardModule, MatToolbarModule, MdDatepickerModule, MdNativeDateModule, MdInputModule, MdButtonModule, MdCheckboxModule } from '@angular/material';
+import {MdTableModule} from '@angular/material';
+
 @NgModule({
     declarations: [
         AppComponent,
@@ -20,11 +31,26 @@ import { MyBookingComponent } from './my-booking/my-booking.component';
     imports: [
         FormsModule,
         RouterModule.forRoot(AppRoutes),
-        HttpModule
+        HttpModule,
+        CdkTableModule,
+        MatTableModule,
+        FlexLayoutModule,
+        BrowserAnimationsModule,
+        BrowserModule,
+        MdNativeDateModule,
+        MdDatepickerModule,
+        MatMenuModule,
+        MdInputModule,
+        MdButtonModule,
+        MdCheckboxModule,
+        MdTableModule,
+        MatToolbarModule,
+        MatCardModule,
+        MatIconModule
     ],
     providers: [],
     exports: [
     ],
     bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
